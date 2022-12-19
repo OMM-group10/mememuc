70d4957e2fa64d5f030cfe4cdf18a1542afc20f9
7vivienl7 <vivien.landgrebe@campus.lmu.de>
2022-12-19 13:19:17 +0100

    Adding subpages

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Editor from './editor';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
+      <div class="navigationBar">
+        <a href="#index"> Index </a>
+        <a id = "editor" href = "./editor" target = "_self"> Editor </a>
+        <a href="#account"> Account </a>
+        <a href="#overview"> Overview </a>
+        <a href="#documentation"> Documentation </a>
+      </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ react-frontend/src/editor.js
@@ -0,0 +1,15 @@
+function Editor(){
+
+  return (
+    <div className="Editor">
+      <header className="Editor-header">
+        <p>
+          Test Editor
+        </p>
+      </header>
+    </div>
+  );
+  }
+
+
+export default Editor;
