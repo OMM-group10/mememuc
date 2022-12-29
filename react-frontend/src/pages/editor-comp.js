c6bd9009b655e09830ba216b976c4bb829825409
Franziska Bumiler <franziska.bumiller@fau.de>
2022-12-29 13:28:51 +0100

    Aufbau einer Editorkomponente

---
+++ react-frontend/src/pages/editor-comp.js
@@ -0,0 +1,38 @@
+// This code is based on the omm-counter.js-file provided in exercise 5
+// and ( ) inspired by the omm-meme-generator presented in exercise 6
+
+// Used structure for a caption in the captions-array in the state
+//interface Caption {
+//    text: string
+//    posX: number
+//    posY: number
+//    col: string
+//    size: number
+//}
+
+import React, {Component} from 'react'
+// import './omm-counter.css'
+export default class EditorComponent extends Component {
+    state = { templateId: -1, user:"", captions:[] }
+
+    addCaption = (inputtext, inputX, inputY, inputcol, inputsize) => 
+        {this.setState({captions: (this.state.captions.push(
+            {text:inputtext, posX:inputX, posY:inputY, col:inputcol, size:inputsize}))})}
+    
+    setTemplate = (inputtemplate) => {this.setState({templateId: (this.state.templateId = inputtemplate)})}
+    setUser = (inputuser) => {this.setState({user: (this.state.user = inputuser)})}
+
+    
+    inc = () => { this.setState({count: (this.state.count + 1)}) }
+    dec = () => { this.setState({count: (this.state.count - 1)}) }
+    render() {
+        return (
+            <div className="counter">
+                <span className="counter-state">{ this.state.count }</span>
+                <div className="buttons">
+                    <button onClick={this.inc}>+</button>
+                    <button onClick={this.dec}>-</button>
+                </div>
+            </div>
+        )
+    }
