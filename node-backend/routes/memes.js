109d663704b183b5288e07f2cb0e5f4dd1894dc8
Alvaxgln <jakob.schmid2@gmail.com>
2022-12-29 02:49:21 +0100

    added functionality for memes specified in URL query

---
+++ node-backend/routes/memes.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  //get collections from db
+  let memes = req.db.get("Meme");
+  let users = req.db.get("Users");
+  let comments = req.db.get("Comments");
+
+  //find specified meme in collection
+  memes.findOne({name: req.query.meme})
+    .then(meme => {
+
+      //find creator    
+      users.findOne({_id: meme.creator})
+        .then(user =>{
+
+          //add creator data to meme
+          meme.creator = user;
+
+          //find comments
+          comments.find({meme: meme._id})
+            .then(comments=>{
+
+              //add comments to meme data
+              meme.comments = comments;
+
+              //return meme data
+              res.json(meme);
+
+            })
+        })
+    })
+
+
+
+  
+
+  /* res.render('index', { title: 'Express' }) */
+});
+
+module.exports = router;
