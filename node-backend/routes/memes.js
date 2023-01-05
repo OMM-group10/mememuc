93c458c3804a548e7ecd831d5467d852fe5dac1a
Alvaxgln <jakob.schmid2@gmail.com>
2023-01-05 12:54:23 +0100

    various changes

--- node-backend/routes/memes.js
+++ node-backend/routes/memes.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET meme information.
+  memes/?meme='titleofmeme'
+*/
 router.get('/', function(req, res, next) {
 
   //get collections from db
@@ -10,7 +12,7 @@ router.get('/', function(req, res, next) {
   let comments = req.db.get("Comments");
 
   //find specified meme in collection
-  memes.findOne({name: req.query.meme})
+  memes.findOne({title: req.query.meme})
     .then(meme => {
 
       //find creator    
@@ -40,6 +42,51 @@ router.get('/', function(req, res, next) {
   
 
   /* res.render('index', { title: 'Express' }) */
+});
+
+
+/* GET a list of all memes
+  /memes/list
+*/
+router.get('/list', function(req, res, next) {
+  //get meme collection
+  let memes = req.db.get("Memes");
+
+  //find memes and return to client
+  memes.find().then(docs=>res.json(docs));
+
+});
+
+//POST data to create a meme and save it in db
+router.post('/create', async function(req, res, next) {
+
+  //get collections
+  let memes = req.db.get("Memes");
+  let users = req.db.get("Users");
+
+  let doc = req.body;
+
+  //find creatorId
+  let user = await users.findOne({username: doc.user});
+  
+  //create new dbDocument
+  doc.creationDate = new Date();
+  doc.creator = user._id;
+  delete doc.user;
+  doc.template = req.db.id(doc.template.$oid)
+
+  //TODO: link meme image/render meme image
+
+  //insert meme in db and return result
+  memes.insert(doc)
+  .then(result=>{
+    res.json(result);
+  })
+
+
+  //res.json(doc);
+  
+
 });
 
 module.exports = router;
