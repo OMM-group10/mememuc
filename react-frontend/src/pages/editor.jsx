7b0a4d4c1321b2c0a36d1b70acec82644ddd0768
7vivienl7 <vivien.landgrebe@campus.lmu.de>
2023-01-05 16:12:29 +0100

    WYSIWYG text is working

--- react-frontend/src/pages/editor.jsx
+++ react-frontend/src/pages/editor.jsx
@@ -3,8 +3,11 @@ import { TextField } from "@mui/material";
 import './navbar.css';
 import './editor.css';
 import Futurama_Meme from './Futurama_Meme.jpeg';
+import React, { useState } from "react";
 
 function Editor() {
+  const [Caption1, setCaption1] = useState("");
+  const [Caption2, setCaption2] = useState("");
   return (
     <div className="Editor">
     <header>
@@ -32,19 +35,35 @@ function Editor() {
           <p> Templates: </p>
         </div>
         <div className="Canvas">
-          <img src={Futurama_Meme}/>
+          <img src={Futurama_Meme} alt="Futurama Meme"/>
+          <p className="Caption1">{Caption1} </p><br></br>
+          <p className="Caption2">{Caption2} </p>
         </div>
         <div className="Params">
           <form>
-            <TextField id="outlined-basic" label="Upper Text" variant="outlined" />
-            <TextField id="outlined-basic" label="X-Position" variant="outlined" />
-            <TextField id="outlined-basic" label="Y-Position" variant="outlined" />
+            <TextField 
+              id="topText"
+              label="Upper Text"
+              variant="outlined"
+              value={Caption1}
+              onChange={(top) => {
+                  setCaption1(top.target.value);
+                }} />
+            <TextField id="topX" label="X-Position" variant="outlined" />
+            <TextField id="topY" label="Y-Position" variant="outlined" />
           </form><br></br>
 
           <form>
-            <TextField id="outlined-basic" label="Lower Text" variant="outlined" />
-            <TextField id="outlined-basic" label="X-Position" variant="outlined" />
-            <TextField id="outlined-basic" label="Y-Position" variant="outlined" />
+            <TextField
+              id="bottomText"
+              label="Lower Text"
+              variant="outlined" 
+              value={Caption2}
+              onChange={(bottom) => {
+                  setCaption2(bottom.target.value);
+                }} />
+            <TextField id="bottomX" label="X-Position" variant="outlined" />
+            <TextField id="bottomY" label="Y-Position" variant="outlined" />
           </form>
         </div>
         </div> 
